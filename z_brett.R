b5d585a2397003e624e3ccd7bfe8d951e1ab02c1
Karl Heilbron <kheilbron@Karls-MacBook-Pro.local>
2023-10-04 11:11:06 +0200

    plot update for poster2

--- z_brett.R
+++ z_brett.R
@@ -745,7 +745,7 @@ magma_plots <- function( maindir, z.or.p="z" ){
     # Initialize the output file
     mag_plot_file <- file.path( mag_plot_dir, paste0( "region_", i, "_", 
                                                       peaks$snp[i], ".jpg" ) )
-    jpeg( filename=mag_plot_file, quality=99, height=240 )
+    jpeg( filename=mag_plot_file, quality=99, height=360 )
     
     # Set up the plot
     ymax <- max(locus$Y) *1.03
@@ -890,7 +890,7 @@ pops_plots <- function( maindir, z.or.p="z" ){
     # Initialize the output file
     pops_plot_file <- file.path( pops_plot_dir, paste0( "region_", i, "_", 
                                                         peaks$snp[i], ".jpg" ) )
-    jpeg( filename=pops_plot_file, quality=99, height=240 )
+    jpeg( filename=pops_plot_file, quality=99, height=360 )
     
     # Set up the plot
     xlab <- paste0( "Chr", peaks$chr[i], " position (Mbp)")
