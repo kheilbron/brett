a82ec897f680eef83244c8d2cf4426b80d638cde
Karl Heilbron <kheilbron@Karls-MacBook-Pro.local>
2023-10-04 13:57:55 +0200

    plot update for poster6

--- z_brett.R
+++ z_brett.R
@@ -745,10 +745,10 @@ magma_plots <- function( maindir, z.or.p="z" ){
     # Initialize the output file
     mag_plot_file <- file.path( mag_plot_dir, paste0( "region_", i, "_", 
                                                       peaks$snp[i], ".jpg" ) )
-    jpeg( filename=mag_plot_file, quality=99, height=320 )
+    jpeg( filename=mag_plot_file, quality=100, height=320 )
     
     # Set up the plot
-    ymax <- max(locus$Y) *1.05
+    ymax <- max(locus$Y) *1.08
     xlab <- paste0( "Chr", peaks$chr[i], " position (Mbp)")
     plot( x=locus$START, y=locus$Y, xlim=c(xmin,xmax), ylim=c(0,ymax),
           xlab=xlab, ylab=ylab, las=1, type="n" )
@@ -890,11 +890,11 @@ pops_plots <- function( maindir, z.or.p="z" ){
     # Initialize the output file
     pops_plot_file <- file.path( pops_plot_dir, paste0( "region_", i, "_", 
                                                         peaks$snp[i], ".jpg" ) )
-    jpeg( filename=pops_plot_file, quality=99, height=320 )
+    jpeg( filename=pops_plot_file, quality=100, height=320 )
     
     # Set up the plot
     ymin <- min( c( 0, locus$Y ) )
-    ymax <- max(locus$Y) * 1.05
+    ymax <- max(locus$Y) * 1.08
     xlab <- paste0( "Chr", peaks$chr[i], " position (Mbp)")
     plot( x=locus$START, y=locus$Y, xlim=c(xmin,xmax), ylim=c(ymin,ymax),
           xlab=xlab, ylab=ylab, las=1, type="n" )
