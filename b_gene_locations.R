110e4088eff8a7c3961319a4a22710ad93741576
Karl Heilbron <karlheilbron@Mac.fritz.box>
2025-08-03 11:58:50 +0200

    allow other GENCODE versions and genome assemblies, output all protein coding genes as well

--- b_gene_locations.R
+++ b_gene_locations.R
@@ -17,8 +17,13 @@ pops <- fread( cmd=pops_cmd )
 #   Load gencode genes, create columns
 #--------------------------------------------------------------------------------
 
-# Read in gencode v44 file
-gc_file <- "/projects/0/prjs0817/projects/pops/data/gencode.v44.grch37.gff3.gz"
+# Read in gencode file
+version  <- 47
+assembly <- 37
+lift_text <- ifelse( assembly == 37,
+                     "lift37", "" )
+gc_file <- paste0( "/projects/0/prjs0817/projects/pops/data/",
+                   "gencode.v", version, lift_text, ".grch", assembly, ".gff3.gz" )
 gc_cmd  <- paste( 'zcat', gc_file, '| grep -v "#"' )
 gc      <- fread( cmd=gc_cmd )
 names(gc) <- c( "CHR", "src", "type", "START", "END", "score", "STRAND", "phase", "attr" )
@@ -61,12 +66,17 @@ gcols <- c( "ENSGID", "CHR", "START", "END", "TSS", "STRAND", "NAME", "TYPE" )
 gc3 <- gc2[ gc2$TYPE %in% gtypes , ..gcols ]
 gc4 <- gc3[ match( pops$ENSGID, gc3$ENSGID ) , ]
 gc4 <- gc4[ !is.na(gc4$ENSGID) , ]
+gc5 <- gc2[ gc2$TYPE == "protein_coding" , ..gcols ]
 
 # Write
-outfile1 <- "/projects/0/prjs0817/projects/pops/data/gene_locations.tsv"
-outfile2 <- "/projects/0/prjs0817/projects/pops/data/gene_locations_inc_nc.tsv"
-fwrite( x=gc4, file=outfile1, sep="\t" )
-fwrite( x=gc3, file=outfile2, sep="\t" )
+outbase <- paste0( "/projects/0/prjs0817/projects/pops/data/",
+                   "gene_locations_gencode_v", version, "_grch", assembly )
+outfile1 <- paste0( outbase, "_inc_nc.tsv" )
+outfile2 <- paste0( outbase, "_all_protein_coding.tsv" )
+outfile3 <- paste0( outbase, "_pops.tsv" )
+fwrite( x=gc3, file=outfile1, sep="\t" )
+fwrite( x=gc5, file=outfile2, sep="\t" )
+fwrite( x=gc4, file=outfile3, sep="\t" )
 
 
 #--------------------------------------------------------------------------------
